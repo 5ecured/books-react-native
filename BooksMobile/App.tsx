7a5fe4c7d25dbff782091fb8d81268622c4c8da2
Edward <onirotto7585@pm.me>
2022-11-22 19:18:24 +1100

    install apollo client

--- BooksMobile/App.tsx
+++ BooksMobile/App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const API_KEY = 'rioreal::stepzen.net+1000::3f6f45bfd7c4c19bdb3898bf857c441f77050688038b24aea14dcbc889b7df2d'
+
+const client = new ApolloClient({
+  uri: "https://rioreal.stepzen.net/api/hopping-bear/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`
+  },
+  cache: new InMemoryCache()
+})
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +25,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
