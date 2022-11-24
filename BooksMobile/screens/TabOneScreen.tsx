53f9914a9b797fdd9fe97f3037ed4fb034e007b4
Edward <onirotto7585@pm.me>
2022-11-24 11:58:26 +1100

    tabs for googlebooks and openlibrary

--- BooksMobile/screens/TabOneScreen.tsx
+++ BooksMobile/screens/TabOneScreen.tsx
@@ -38,6 +38,7 @@ const query = gql`
 
 export default function TabOneScreen() {
   const [search, setSearch] = useState<string>('')
+  const [provider, setProvider] = useState<'googleBooksSearch' | 'openLibrarySearch'>('googleBooksSearch')
 
   // useQuery automatically runs when component mounts, or variables change etc.
   // const { data, loading, error } = useQuery(query, { variables: { q: search } })
@@ -45,6 +46,24 @@ export default function TabOneScreen() {
   // In order to make a graphQL request after a manual trigger e.g. when we press a button, we use useLazyQuery
   // Pay attention to the format, its a bit different from useQuery. "runQuery" is the function we call to trigger it
   const [runQuery, { data, loading, error }] = useLazyQuery(query)
+
+  const parseBook = (item: any): Book => {
+    if (provider === 'googleBooksSearch') {
+      return {
+        image: item.volumeInfo.imageLinks?.thumbnail,
+        title: item.volumeInfo.title,
+        authors: item.volumeInfo.authors,
+        isbn: item.volumeInfo.industryIdentifiers[0].identifier
+      }
+    }
+
+    return {
+      image: `https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`,
+      title: item.title,
+      authors: item.author_name,
+      isbn: item.isbn?.[0]
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -61,6 +80,21 @@ export default function TabOneScreen() {
         />
       </View>
 
+      <View style={styles.tabs}>
+        <Text
+          style={provider === 'googleBooksSearch' ? { fontWeight: 'bold', color: 'royalblue' } : {}}
+          onPress={() => setProvider('googleBooksSearch')}
+        >
+          Google Books
+        </Text>
+        <Text
+          style={provider === 'openLibrarySearch' ? { fontWeight: 'bold', color: 'royalblue' } : {}}
+          onPress={() => setProvider('openLibrarySearch')}
+        >
+          Open Library
+        </Text>
+      </View>
+
       {loading && <ActivityIndicator />}
       {error && (
         <>
@@ -69,14 +103,11 @@ export default function TabOneScreen() {
         </>
       )}
       <FlatList
-        data={data?.googleBooksSearch?.items || []}
+        data={(provider === 'googleBooksSearch' ? data?.googleBooksSearch?.items : data?.openLibrarySearch?.docs) || []}
         renderItem={({ item }) => (
-          <BookItem book={{
-            image: item.volumeInfo.imageLinks?.thumbnail,
-            title: item.volumeInfo.title,
-            authors: item.volumeInfo.authors,
-            isbn: item.volumeInfo.industryIdentifiers[0].identifier
-          }} />
+          <BookItem
+            book={parseBook(item)}
+          />
         )}
         showsVerticalScrollIndicator={false}
       />
@@ -109,5 +140,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginVertical: 5
+  },
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 50,
+    alignItems: 'center'
   }
 });
