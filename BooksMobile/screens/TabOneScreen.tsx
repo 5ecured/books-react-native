376001b9609cb2942c3b7a44d0deea9c4b33e717
Edward <onirotto7585@pm.me>
2022-11-24 11:29:34 +1100

    search input box

--- BooksMobile/screens/TabOneScreen.tsx
+++ BooksMobile/screens/TabOneScreen.tsx
@@ -1,7 +1,8 @@
-import { ActivityIndicator, StyleSheet, FlatList } from 'react-native';
+import { ActivityIndicator, StyleSheet, FlatList, TextInput, Button } from 'react-native';
 import { Text, View } from '../components/Themed';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useLazyQuery } from '@apollo/client';
 import BookItem from '../components/BookItem';
+import { useState } from 'react';
 
 const query = gql`
   query SearchBooks($q: String) {
@@ -36,10 +37,30 @@ const query = gql`
 `;
 
 export default function TabOneScreen() {
-  const { data, loading, error } = useQuery(query, { variables: { q: 'React Native' } })
+  const [search, setSearch] = useState<string>('')
+
+  // useQuery automatically runs when component mounts, or variables change etc.
+  // const { data, loading, error } = useQuery(query, { variables: { q: search } })
+
+  // In order to make a graphQL request after a manual trigger e.g. when we press a button, we use useLazyQuery
+  // Pay attention to the format, its a bit different from useQuery. "runQuery" is the function we call to trigger it
+  const [runQuery, { data, loading, error }] = useLazyQuery(query)
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <TextInput
+          value={search}
+          placeholder='Search...'
+          style={styles.input}
+          onChangeText={setSearch}
+        />
+        <Button
+          title='Search'
+          onPress={() => runQuery({ variables: { q: search } })}
+        />
+      </View>
+
       {loading && <ActivityIndicator />}
       {error && (
         <>
@@ -77,4 +98,16 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gainsborough',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5
+  }
 });
