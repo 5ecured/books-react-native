4fdd6af14f9bd681d9a115076757f82e918dbf72
Edward <onirotto7585@pm.me>
2022-12-04 15:20:12 +1100

    bugfixes

--- BooksMobile/App.tsx
+++ BooksMobile/App.tsx
@@ -7,12 +7,10 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import MyBooksProvider from './context/MyBooksProvider';
 
-const API_KEY = 'rioreal::stepzen.net+1000::3f6f45bfd7c4c19bdb3898bf857c441f77050688038b24aea14dcbc889b7df2d'
-
 const client = new ApolloClient({
   uri: "https://rioreal.stepzen.net/api/hopping-bear/__graphql",
   headers: {
-    Authorization: `Apikey ${API_KEY}`
+    Authorization: `Apikey ${process.env.REACT_APP_API_KEY}`
   },
   cache: new InMemoryCache()
 })
